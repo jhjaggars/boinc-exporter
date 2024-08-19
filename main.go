be013be7ace94615fab8592c8770c4cf9dd3c706
Jesse Jaggars <jhjaggars@gmail.com>
2024-08-19 12:33:56 -0400

    tailing logs

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
+	"github.com/nxadm/tail"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var clientStatePath string = "/var/lib/boinc-client/client_state.xml"
+var hostname string = "localhost"
 
 var boinc_hostinfo_domainname = promauto.NewSummary(prometheus.SummaryOpts{
 	Name: "boinc_hostinfo_domainname",
@@ -29,6 +34,11 @@ var boinc_result_received_time = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Help: "unix time received",
 }, []string{"name"})
 
+var boinc_active_task_count = promauto.NewGauge(prometheus.GaugeOpts{
+	Name: "boinc_active_task_count",
+	Help: "current number of tasks",
+})
+
 var boinc_active_task_fraction_done = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Name: "boinc_active_task_fraction_done",
 	Help: "percentage of task completed",
@@ -38,6 +48,31 @@ var boinc_active_task_elapsed_time = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Name: "boinc_active_task_elapsed_time",
 	Help: "time spent working on active task",
 }, []string{"name"})
+
+var boinc_task_assigned = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "boinc_task_assigned",
+	Help: "task assignments",
+})
+
+var boinc_task_started = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "boinc_task_started",
+	Help: "task starting",
+})
+
+var boinc_task_completed = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "boinc_task_completed",
+	Help: "task completed",
+})
+
+var boinc_task_uploaded = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "boinc_task_uploaded",
+	Help: "task uploaded",
+})
+
+var boinc_task_downloaded = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "boinc_task_downloaded",
+	Help: "task downloaded",
+})
 
 type HostInfo struct {
 	DomainName string `xml:"domain_name"`
@@ -89,12 +124,71 @@ func syncMiddleware(next http.Handler) http.Handler {
 			boinc_result_deadline.WithLabelValues(result.Name).Set(result.ReportDeadline)
 			boinc_result_received_time.WithLabelValues(result.Name).Set(result.ReportDeadline)
 		}
+		boinc_active_task_count.Set(float64(len(state.ActiveTaskSet.ActiveTasks)))
 		for _, task := range state.ActiveTaskSet.ActiveTasks {
 			boinc_active_task_elapsed_time.WithLabelValues(task.Name).Set(task.ElapsedTime)
 			boinc_active_task_fraction_done.WithLabelValues(task.Name).Set(task.FractionDone)
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+// Scheduler request completed: got 0 new tasks
+var intRe = regexp.MustCompile(`\d+`)
+
+type addFunc func(line string) (int, error)
+
+func getInt(line string) (int, error) {
+	return strconv.Atoi(intRe.FindString(line))
+}
+
+func addOne(_ string) (int, error) {
+	return 1, nil
+}
+
+func addIntFrom(line, sub string, counter prometheus.Counter, adder addFunc) bool {
+	if idx := strings.Index(line, sub); idx > -1 {
+		num, err := adder(line[idx:])
+		if err != nil {
+			log.Printf("expected integer in substring '%s', but didn't find one: %s", sub, err.Error())
+			return true
+		}
+		counter.Add(float64(num))
+		return true
+	}
+	return false
+}
+
+func logLineParse(line string) {
+
+	if addIntFrom(line, "Scheduler request complete: got", boinc_task_assigned, getInt) {
+		return
+	}
+	if addIntFrom(line, "Starting task", boinc_task_started, addOne) {
+		return
+	}
+	if addIntFrom(line, "Computation for task", boinc_task_completed, addOne) {
+		return
+	}
+	if addIntFrom(line, "Finished upload of", boinc_task_uploaded, addOne) {
+		return
+	}
+	if addIntFrom(line, "Finished download of", boinc_task_downloaded, addOne) {
+		return
+	}
+}
+
+func follow(logfilePath string) {
+	t, err := tail.TailFile(logfilePath, tail.Config{
+		Follow: true,
+		ReOpen: true,
+	})
+	if err != nil {
+		log.Printf("failed to tail logfile, no metrics will be collected.: %s", err.Error())
+	}
+	for line := range t.Lines {
+		logLineParse(line.Text)
+	}
 }
 
 func main() {
@@ -111,5 +205,13 @@ func main() {
 	if port == "" {
 		port = "9100"
 	}
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	logfilePath := os.Getenv("BOINC_LOGFILE_PATH")
+	if logfilePath != "" {
+		go follow(logfilePath)
+	}
+	log.Printf("boinc-exporter starting on :%s", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		log.Printf("http server failed: %s", err.Error())
+	}
 }
